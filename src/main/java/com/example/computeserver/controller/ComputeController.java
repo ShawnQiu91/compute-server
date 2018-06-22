8d5685676df13b4447899a28c1a91499dd5a8fe0
TimQiu91 <123040844@qq.com>
2018-06-22 16:59:28 +0800

    20180622 update

--- src/main/java/com/example/computeserver/controller/ComputeController.java
+++ src/main/java/com/example/computeserver/controller/ComputeController.java
@@ -1,6 +1,8 @@
 package com.example.computeserver.controller;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,10 @@ import java.util.Random;
 
 @RestController
 @RequestMapping
+@RefreshScope
 public class ComputeController {
+    @Value("${profile}")
+    private String profile;
 
     /**
      * 获取一个随机整数
@@ -19,5 +24,11 @@ public class ComputeController {
     @ApiOperation(value = "获取一个随机整数", notes = "", produces = "application/json;charset=UTF-8")
     public int geneNumber(){
         return new Random().nextInt();
+    }
+
+    @GetMapping("/v1/profile")
+    @ApiOperation(value = "获取一个配置参数", notes = "", produces = "application/json;charset=UTF-8")
+    public String viewProfile(){
+        return profile;
     }
 }
