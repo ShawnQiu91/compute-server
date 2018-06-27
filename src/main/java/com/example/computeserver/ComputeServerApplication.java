1db01275a6cac36334f8601eb463d0e59522e6fc
TimQiu91 <123040844@qq.com>
2018-06-27 10:52:54 +0800

    add ribbon update

--- src/main/java/com/example/computeserver/ComputeServerApplication.java
+++ src/main/java/com/example/computeserver/ComputeServerApplication.java
@@ -3,13 +3,20 @@ package com.example.computeserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableEurekaClient
 public class ComputeServerApplication {
 
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ComputeServerApplication.class, args);
 	}
--- src/main/java/com/example/computeserver/controller/ComputeController.java
+++ src/main/java/com/example/computeserver/controller/ComputeController.java
@@ -1,11 +1,13 @@
 package com.example.computeserver.controller;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Random;
 
@@ -15,6 +17,8 @@ import java.util.Random;
 public class ComputeController {
     @Value("${profile}")
     private String profile;
+    @Autowired
+    private RestTemplate restTemplate;
 
     /**
      * 获取一个随机整数
@@ -30,5 +34,15 @@ public class ComputeController {
     @ApiOperation(value = "获取一个配置参数", notes = "", produces = "application/json;charset=UTF-8")
     public String viewProfile(){
         return profile;
+    }
+
+    /**
+     * 问好
+     * @return
+     */
+    @GetMapping("/v1/hello")
+    @ApiOperation(value = "问好", notes = "", produces = "application/json;charset=UTF-8")
+    public String hello(){
+        return restTemplate.getForEntity("http://compute-service/v1/hello", String.class).getBody();
     }
 }
