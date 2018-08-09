d07154bb916f2d820e3104c430693b2bcb8b80cd
TimQiu91 <123040844@qq.com>
2018-08-09 10:51:18 +0800

    增加多线程-lock Demo

---
+++ src/main/java/com/example/computeserver/multithread/ReenTrantLockDemo.java
@@ -0,0 +1,80 @@
+package com.example.computeserver.multithread;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 与synchronized相比ReenTrantLock是加在对象（类、实例）上，还是临界资源上？
+ * 结果：
+ * 1、当两个线程同时请求同一临界资源时，产生同步控制
+ * 2、当两个线程同时请求同一类对象的不同临界资源时(用不同的lock)，不会产生同步控制
+ * 3、当两个线程同时请求同一类对象的不同临界资源时(用相同的lock)，产生同步控制
+ * 结论：
+ * 1、被同一个lock加锁的不同资源被多线程请求时会产生同步控制（lock对同一个线程可重入，对多个线程不可重入）
+ * 2、被不同lock加锁的不同资源被多线程请求时不会产生同步控制
+ * 3、与synchronized相比，synchronized锁是加在整个对象（类或实例）上，而lock锁是加在被lock-unlock包围的块上。
+ */
+public class ReenTrantLockDemo {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new ReenTrant(0));
+        Thread t2 = new Thread(new ReenTrant(1));
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("num : "+ ReenTrant.getN());
+    }
+}
+
+class ReenTrant implements Runnable {
+    private int flag;
+    private static ReentrantLock lock1 = new ReentrantLock();
+    private static ReentrantLock lock2 = new ReentrantLock();
+    private static int n;
+
+    public static int getN() {
+        return n;
+    }
+
+    public ReenTrant(int flag) {
+        this.flag = flag;
+    }
+
+    @Override
+    public void run() {
+        if (flag == 0) {
+            m1();
+        } else {
+            m2();
+        }
+
+    }
+
+    public static void m1() {
+        for (int i = 0; i < 100000; i++) {
+            try {
+                lock1.lock();
+                n++;
+            } finally {
+                lock1.unlock();
+            }
+        }
+    }
+
+    public static void m2() {
+        for (int i = 0; i < 100000; i++) {
+            try {
+                lock2.lock();
+                n++;
+            } finally {
+                lock2.unlock();
+            }
+        }
+    }
+}
+
+
--- src/main/java/com/example/computeserver/multithread/SynchronizedDemo.java
+++ src/main/java/com/example/computeserver/multithread/SynchronizedDemo.java
@@ -6,28 +6,22 @@ package com.example.computeserver.multithread;
  * 线程1执行类的一个加锁方法a，获取到类锁未释放，同时线程2访问类的另一个加锁方法b，b会在a执行完成后执行。
  * 若线程2访问的是非加锁方法，则可以正常执行，只有访问临界区时才需要检查线程是否持有对象的锁。
  * 结论：1、虽然锁是加在类或对象上，但只有多个线程同时访问同一个锁下的 *临界资源* 时才会进行同步控制
- *       2、访问同一锁下的不同*临界资源*时也需要同步控制
+ * 2、访问同一锁下的不同*临界资源*时也需要同步控制
+ * 3、可以把synchronized看作统一的lock（多个临界资源上加synchronized，相当于加同一个lock）
  */
 public class SynchronizedDemo implements Runnable {
+    private static int n = 0;
 
     public static synchronized void increasea() {
-        System.out.println(" a : start");
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 0; i < 100000; i++) {
+            n++;
         }
-        System.out.println(" a : end");
     }
 
     public static synchronized void increaseb() {
-        System.out.println(" b : start");
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 0; i < 100000; i++) {
+            n++;
         }
-        System.out.println(" b : end");
     }
 
     @Override
@@ -54,5 +48,6 @@ public class SynchronizedDemo implements Runnable {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        System.out.println("num : "+ n);
     }
 }
