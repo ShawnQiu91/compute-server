234414e8f2e468b986156570c6d28b4f78908f1b
TimQiu91 <123040844@qq.com>
2018-08-24 16:19:00 +0800

    mybatis-实战示例

--- src/main/java/com/example/computeserver/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/computeserver/service/impl/UserServiceImpl.java
@@ -7,10 +7,12 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService{
     @Autowired
     private UserMapper userMapper;//这里会报错，但是并不会影响
