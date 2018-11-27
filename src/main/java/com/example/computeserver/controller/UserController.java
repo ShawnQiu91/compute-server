72928e1b06502a3aa413d1ef8d10396ba6fcc034
TimQiu91 <123040844@qq.com>
2018-11-27 12:02:59 +0800

    模拟SSO返回token

--- src/main/java/com/example/computeserver/controller/UserController.java
+++ src/main/java/com/example/computeserver/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.computeserver.controller;
 
+import com.example.computeserver.controller.dto.UserDTO;
 import com.example.computeserver.model.User;
 import com.example.computeserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,24 @@ public class UserController {
     private UserService userService;
 
     @PutMapping(value = "/user", produces = {"application/json;charset=UTF-8"})
-    public int addUser(@RequestBody User user){
+    public int addUser(@RequestBody User user) {
         return userService.addUser(user);
     }
 
 
     @GetMapping(value = "/users/{pageNum}/{pageSize}", produces = {"application/json;charset=UTF-8"})
-    public Object findAllUser(@PathVariable("pageNum") int pageNum, @PathVariable("pageSize") int pageSize){
-        return userService.findAllUser(pageNum,pageSize);
+    public Object findAllUser(@PathVariable("pageNum") int pageNum, @PathVariable("pageSize") int pageSize) {
+        return userService.findAllUser(pageNum, pageSize);
+    }
+
+    @GetMapping(value = "/api/tokenapi/GetUser")
+    @ResponseBody
+    public UserDTO getUser(@RequestParam("token") String token) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setToken(token);
+        userDTO.setPassWord("123456");
+        userDTO.setIsExpired(false);
+        userDTO.setUserCode("misa");
+        return userDTO;
     }
 }
